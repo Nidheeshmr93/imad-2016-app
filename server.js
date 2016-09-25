22c2e74caedfbaed88a1c7f677533549d17e0bee
Nidheeshmr93 <nidheeshmr93@gmail.com>
2016-09-25 12:39:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,63 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne = {
+  title:'Article-One | Nidheesh',
+  haeding:'Article One',
+  date: 'Sep 20, 2016',
+  content:`<p>
+                 In many languages, articles are a special part of speech, which cannot easily be combined with other parts of speech. In English, articles are frequently considered a part of a broader speech category called determiners, which combines articles and demonstratives (such as "this" and "that").
+            </p>
+            <p>
+                In many languages, articles are a special part of speech, which cannot easily be combined with other parts of speech. In English, articles are frequently considered a part of a broader speech category called determiners, which combines articles and demonstratives (such as "this" and "that").
+            </p>
+            <p>
+                In many languages, articles are a special part of speech, which cannot easily be combined with other parts of speech. In English, articles are frequently considered a part of a broader speech category called determiners, which combines articles and demonstratives (such as "this" and "that").
+            </p>`
+};
+function createTemplate (data) {
+var title = data.title;
+var date = data.date;
+var heading = data.heading;
+var content=data.content;
+
+var htmlTemplate = `<html>
+<head>
+   <title>
+     ${title}
+   </title>
+   <meta name="viewport" content="width=device-width, initial-scale=1" />
+   <link href="/ui/style.css" rel="stylesheet" />
+</head>
+<body>	
+    <div class="container">
+	    <div>
+            <a href="/">Home</a>
+        </div>
+        <hr/>
+            <h3>
+              ${heading}
+            </h3>
+        <div>
+           ${date}
+        </div>
+        <div>
+          ${ontent}
+        </div>
+    </div>
+</body>
+</html>
+`;
+return  htmlTemplate;
+}
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one' , function (req,res) {
- res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+ res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two' , function (req,res) {
